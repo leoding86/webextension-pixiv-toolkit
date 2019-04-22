0a4631b7efbf66dbb8c4bf38d26cb1a7d3a71af9
leoding86 <leoding86@gmail.com>
2019-04-22 16:31:34 +0800

    rename NovalAdapter to NovelAdapter

--- src/statics/js/NovalAdapter.js
+++
@@ -1,9 +0,0 @@
-_pumd.NovalAdapter = (function(common) {
-    var NovalAdapter = {};
-
-    NovalAdapter.getNovalInstance = function() {
-        return new Noval190115();
-    };
-
-    var noval = NovalAdapter.getNovalInstance();
-})(_pumd.common);
