c006f969addfbc287052ee84b0c12a777cc80566
leoding86 <leoding86@gmail.com>
2019-06-15 14:50:00 +0800

    add PlusNotice.vue

---
+++ src/options_page/components/PlusNotice.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="plus-notice__wrap">
+    <p class="plus-notice__p">This feature requires the Pixiv Toolkit Plus Addon, which you can purchase at the <a target="_blank">Chrome Store</a><v-icon small>open_in_new</v-icon> or by supporting me on <a href="https://www.patreon.com/leoding" target="_blank">Patreon</a><v-icon small>open_in_new</v-icon>.</p>
+    <p class="plus-notice__p">I'll develop more features/apps for my patrons. Anyway, thanks for using this extension.</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.plus-notice__p {
+  font-size: 16px;
+  line-height: 26px;
+  margin: 0;
+}
+</style>
