1f2464de823ee8b70ffe68b58026c65b7d14edb4
leoding86 <leoding86@gmail.com>
2019-06-15 14:49:52 +0800

    add pageTitle.vue

---
+++ src/options_page/components/PageTitle.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="page-title__wrap">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.page-title__wrap {
+  padding: 10px 0;
+}
+</style>
