d87a84247beeffe77e54e22a4f25e1ef6d1f1030
leoding86 <leoding86@gmail.com>
2019-06-19 19:57:24 +0800

    update PlusNotice.vue

--- src/options_page/components/PlusNotice.vue
+++ src/options_page/components/PlusNotice.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="plus-notice__wrap">
-    <p class="plus-notice__p">This feature requires the Pixiv Toolkit Plus Addon, which you can get by supporting me on <a href="https://www.patreon.com/leoding" target="_blank">Patreon</a><v-icon small>open_in_new</v-icon>. I'll develop more features/apps for my patrons. </p>
+    <p class="plus-notice__p">This feature requires the <strong>Pixiv Toolkit Plus Addon</strong>, which you can get by supporting me on <a href="https://www.patreon.com/leoding" target="_blank">Patreon</a><v-icon small>open_in_new</v-icon>.</p>
+    <p class="plus-notice__p">With <strong>Pixiv Toolkit Plus Addon</strong>, you can get additional features, such as, save illust visit history as many as you visit.</p>
     <p class="plus-notice__p">Anyway, thanks for using this extension.</p>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .plus-notice__p {
+  margin: 20px 0;
   font-size: 16px;
   line-height: 26px;
-  margin: 0;
 }
 </style>
