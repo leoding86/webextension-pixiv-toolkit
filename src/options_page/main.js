19715a914ee209f4c60eddc1b252b7b4b24263b8
leoding86 <leoding86@gmail.com>
2019-08-13 18:02:39 +0800

    Remove plus addon related codes

--- src/options_page/main.js
+++ src/options_page/main.js
@@ -4,21 +4,12 @@ import router from './router'
 import cr from './modules/cr'
 import { Storage as BrowserStorage } from '@/modules/Browser'
 import Browser from '@/modules/Browser/Browser'
-import PlusAddonPort from '@/modules/PlusAddonPort'
 
-let browser = window.browser = Browser.getBrowser()
+window.browser = Browser.getBrowser()
 
 let browserStorage = BrowserStorage.getInstance()
 
 let thisApp = window.thisApp = {}
-
-let plusAddonPort
-
-plusAddonPort = window.plusAddonPort = new PlusAddonPort('options_page')
-
-if (browser.runtime.lastError) {
-  plusAddonPort = null
-}
 
 window.browserItems = {}
 
@@ -44,26 +35,16 @@ cr._s.get().then(function (items) {
       /* eslint-disable no-new */
       new Vue({
         el: '#app',
+
         router,
+
         render: h => h(App),
+
         data() {
           return {
             plusVersion: null
           }
-        },
-        beforeMount() {
-          let vm = this
-
-          if (plusAddonPort) {
-            plusAddonPort.port.onMessage.addListener((message, port) => {
-              if (message && message.version) {
-                vm.plusVersion = message.version
-              }
-            })
-
-            plusAddonPort.checkPlusAddonInstalled()
-          }
-        },
+        }
       })
     })
 });
