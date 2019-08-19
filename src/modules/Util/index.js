fd008e24c05f4fb2450a164d6a566374f521178f
leoding86 <leoding86@gmail.com>
2019-08-19 15:27:30 +0800

    remove Magic module

--- src/modules/Util/index.js
+++ src/modules/Util/index.js
@@ -1,10 +1,8 @@
-import Magic from './Magic';
 import ScriptInjector from './ScriptInjector';
 import PackageFileReader from './PackageFileReader';
 import Updater from './Updater';
 
 export {
-  Magic,
   ScriptInjector,
   PackageFileReader,
   Updater
