c01fd503839cc24cc554d9cd3260174e95db3bfc
leoding86 <leoding86@gmail.com>
2019-09-04 16:58:18 +0800

    add AssetCache.js

---
+++ src/repositories/AssetCache.js
@@ -0,0 +1,70 @@
+import PouchDB from 'pouchdb';
+import PouchDBFind from 'pouchdb-find';
+
+PouchDB.plugin(PouchDBFind)
+
+class AssetCache {
+  constructor() {
+    this.db = new PouchDB('asset_caches');
+
+    this.maxLimit = 150;
+
+    this.init();
+  }
+
+  init() {
+    return this.db.createIndex({
+      index: {
+        fields: ['cached_at']
+      }
+    })
+  }
+
+  checkLimitationAndRemove() {
+    this.db.allDocs().then(entries => {
+      let deleteLength = entries.rows.length - this.maxLimit;
+
+      if (deleteLength > 0) {
+        this.db.find({
+          selector: { cached_at: { $gte: null } },
+          sort: [{ 'cached_at': "asc"}],
+          limit: deleteLength
+        }).then(response => {
+          response.docs.forEach(doc => {
+            this.db.remove(doc).catch(err => {
+              console.log(err);
+            });
+          })
+        })
+      }
+    })
+  }
+
+  putAsset(id, asset) {
+    this.checkLimitationAndRemove();
+
+    return new Promise(resolve => {
+      this.db.get(id).then(() => {
+        resolve();
+      }).catch(() => {
+        resolve(this.db.put({
+          _id: id,
+          asset: asset,
+          cached_at: parseInt((Date.now() + '').substr(0, 10))
+        }));
+      });
+    });
+  }
+
+  retrieveAsset(id) {
+    return new Promise((resolve, reject) => {
+      this.db.get(id).then(doc => {
+        resolve(doc);
+      }).catch(err => {
+        reject(err);
+      });
+    });
+  }
+}
+
+export default AssetCache;
