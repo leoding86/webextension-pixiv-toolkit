79cab530945f68462147962630fd86b82a8be7c9
leoding86 <leoding86@gmail.com>
2019-09-05 15:11:56 +0800

    update sponsors page

--- src/options_page/components/Sponsors.vue
+++ src/options_page/components/Sponsors.vue
@@ -7,7 +7,10 @@
         <p style="font-size:14px;">{{ tl('sponsor_update_periodically') }} ~ヾ(＾∇＾)</p>
         <p style="font-size:14px;" v-if="sponsors === null">Loading sponsors...</p>
         <ul v-else-if="sponsors.length > 0">
-          <li v-for="(sponsor, i) in sponsors" :key="i" :class="'sponsor sponsor__level-' + sponsor.level">{{ sponsor.name }}</li>
+          <li v-for="(sponsor, i) in sponsors" :key="i" :class="'sponsor sponsor__level-' + sponsor.level">
+            <span class="sponser__glitter-container"></span>
+            <span class="sponser__name">{{ sponsor.name }}</span>
+          </li>
         </ul>
         <p class="no-sponsor" v-else>{{ tl('sponsor_very_first') }} ~ヾ(＾∇＾)</p>
       </v-card-text>
@@ -33,7 +36,7 @@ export default {
 
   mounted () {
     let vm = this,
-        sponsorsUrl = 'https://raw.githubusercontent.com/leoding86/pixiv-toolkits/master/src/statics/sponsors.json',
+        sponsorsUrl = 'https://raw.githubusercontent.com/leoding86/webextension-pixiv-toolkit/master/src/statics/sponsors.json',
         xhr = new XMLHttpRequest();
 
     xhr.open('get', sponsorsUrl);
@@ -72,7 +75,11 @@ ul {
   }
 }
 
+$text-color: #ffed21;
+$text-shadow: 2px 2px 0 #df7c11;
+
 .sponsor {
+
   display: inline-block;
   font-size: 16px;
   border: 1px solid #ccc;
@@ -80,6 +87,11 @@ ul {
   margin-right: 10px;
   border-radius: 100px;
   background: #fff;
+  box-shadow: 3px 3px 0px #ccc;
+
+  .sponser__name {
+    position: relative;
+  }
 }
 
 .sponsor__level-1 {
@@ -88,5 +100,26 @@ ul {
 
 .sponsor__level-2 {
   font-weight: 1000;
+  background: #585c56;
+  color: $text-color;
+  text-shadow: $text-shadow;
+}
+
+.sponsor__level-3 {
+  position: relative;
+  font-weight: 1000;
+  background: #585c56;
+  color: $text-color;
+  text-shadow: $text-shadow;
+
+  .sponser__glitter-container {
+    display: block;
+    position: absolute;
+    top: -20%;
+    left: -10%;
+    width: 120%;
+    height: 140%;
+    background: url(../assets/glitter.gif) 0 -10px;
+  }
 }
 </style>
