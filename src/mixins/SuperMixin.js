107b2a69bd697b5ebe8be880303d0e8ef8e3a401
leoding86 <leoding86@gmail.com>
2019-11-06 15:57:50 +0800

    add `stripTags` method

--- src/mixins/SuperMixin.js
+++ src/mixins/SuperMixin.js
@@ -8,6 +8,14 @@ export default {
   methods: {
     tl(string) {
       return browser.i18n.getMessage(string);
+    },
+
+    stripTags(content, replace = ' ') {
+      if (typeof content !== 'string') {
+        return content;
+      }
+
+      return content.replace(/<\/?[^>]+(>|$)/g, replace.repeat(2)).trim();
     }
   }
 }
