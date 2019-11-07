bb3ca23bb51eb0aea9d217ce91ad1d7aaa6fe7ec
leoding86 <leoding86@gmail.com>
2019-11-07 20:34:46 +0800

    update `Test.vue`

--- src/options_page/components/Test.vue
+++ src/options_page/components/Test.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="test">
-    <h1>test page</h1>
+  <div class="container container--big">
+    <h1>Test cacheable component</h1>
     <cacheable-image style="width:100px;height:100px;"
       mode="background"
       :src="image"></cacheable-image>
+    <h1>Debug options</h1>
+    <div>
+      <button @click="resetVersionNumber">Reset version number</button>
+    </div>
   </div>
 </template>
 
@@ -18,6 +22,19 @@ export default {
   data() {
     return {
       image: 'https://s.pximg.net/www/images/logo/pixiv-logo.svg'
+    }
+  },
+
+  methods: {
+    resetVersionNumber() {
+      browser.storage.local.set({
+        version: '0.0.1'
+      }, () => {
+        browser.storage.local.get(null, items => {
+          console.log(items);
+          alert(items.version);
+        });
+      });
     }
   }
 }
