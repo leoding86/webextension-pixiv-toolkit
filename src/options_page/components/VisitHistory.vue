551a441098f3bf87a4272ac81ebe836b55ea3c4c
leoding86 <leoding86@gmail.com>
2019-11-14 18:12:47 +0800

    update

--- src/options_page/components/VisitHistory.vue
+++ src/options_page/components/VisitHistory.vue
@@ -5,16 +5,18 @@
       type="warning"
       v-if="!enableSaveVisitHistory">Save visit history has been disabled</v-alert>
 
-    <!-- Searchbar -->
-    <v-text-field class="search-panel"
-      label="Solo"
-      single-line
-      solo
-      flat
-      placeholder="Search History (Beta)"
-      v-model="searchQuery"
-    ></v-text-field>
-    <!-- /Searchbar -->
+    <div class="visit-history__header">
+      <!-- Searchbar -->
+      <v-text-field class="search-panel"
+        label="Solo"
+        single-line
+        solo
+        flat
+        placeholder="Search History (Beta)"
+        v-model="searchQuery"
+      ></v-text-field>
+      <!-- /Searchbar -->
+    </div>
 
     <div class="dataset-head-actions-wrap">
       <v-btn
@@ -23,47 +25,16 @@
         depressed
         flat>Total {{ total }} Records</v-btn>
 
-      <v-btn @click="exportIllustHistory"
-        depressed
-        color="#eee">
-        Export
-        <v-progress-circular
-          indeterminate
-          color="primary"
-          v-if="exporting"
-          size="20"
-          width="3"
-          style="margin-left:5px"
-        ></v-progress-circular>
-      </v-btn>
-
-      <v-btn
-        @click="importIllustHistory"
-        depressed
-        color="#eee">
-        Import <span v-if="importTotal > 0">({{ importedCount }} / {{ importTotal }})</span>
-        <v-progress-circular
-          indeterminate
-          color="primary"
-          v-if="importing"
-          size="20"
-          width="3"
-          style="margin-left:5px"
-        ></v-progress-circular>
-      </v-btn>
-
       <v-switch v-model="disableBlurOnR" label="Disable mask"></v-switch>
 
       <!-- <v-btn @click="insertData">Insert 10w</v-btn> -->
     </div>
 
     <v-layout row wrap
-      class="illust-history-wrap">
-      <p v-if="illusts.length <= 0"
-        style="font-size:14px;text-align:center;">
-        There is no any history
-      </p>
-      <recycle-scroller v-else
+      class="illust-history-wrap"
+      v-if="illusts.length > 0"
+    >
+      <recycle-scroller
         class="scroller"
         :items="illusts"
         :item-size="90"
@@ -72,8 +43,11 @@
         v-slot="{ item }"
       >
         <div class="illust-content">
-          <div class="illust-content__thumb-content">
+          <div class="illust-content__thumb-content"
+            @click="openInNew(item)"
+          >
             <cacheable-image class="illust-content__thumb"
+              :class="{'illust-content__thumb--blur': item.r && !disableBlurOnR}"
               :src="item.images.thumb"
               mode="background"
             ></cacheable-image>
@@ -81,34 +55,31 @@
           <div class="illust-content__info">
             <div class="illust-content__info-entity illust-content__info-entity--blod"><span class="illust-content__badge" :class="`illust-content__badge--type${item.type}`">{{ caseWorkType(item.type) }}</span>{{ item.title }}</div>
             <div class="illust-content__info-entity">{{ caseDate(item.viewed_at) }}</div>
-            <div class="illust-content__info-entity">{{ caseWorkUrl(item.id) }}</div>
+            <div class="illust-content__info-entity illust-content__info-entity--sub">
+              <a :href="caseWorkUrl(item.id)">{{ caseWorkUrl(item.id) }}</a>
+            </div>
           </div>
           <div class="illust-content__actions">
-            <v-menu left bottom>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  class="illust-content__actions-btn"
-                  flat
-                  icon
-                  v-on="on"
-                >
-                  <v-icon>more_vert</v-icon>
-                </v-btn>
-              </template>
-              <v-list>
-                <v-list-tile>
-                  <v-list-tile-title
-                    @click="deleteOne(item)"
-                  >Delete</v-list-tile-title>
-                </v-list-tile>
-              </v-list>
-            </v-menu>
+            <v-btn
+              class="illust-content__actions-btn"
+              flat
+              icon
+              @click="deleteOne(item)"
+            >
+              <v-icon>delete</v-icon>
+            </v-btn>
           </div>
         </div>
       </recycle-scroller>
     </v-layout>
 
-    <div style="text-align:center;clear:both;" v-if="loading">
+    <p v-if="statusNotice"
+      class="visit-history__status-notice"
+    >
+      {{ statusNotice }}
+    </p>
+
+    <div style="text-align:center;clear:both;margin-top:20px;" v-if="loading">
       <v-progress-circular
         indeterminate
         color="primary"
@@ -157,18 +128,13 @@ export default {
     return {
       total: 0,
       illusts: [],
-      page: 0,
-      step: 36,
+      offset: 0,
+      step: 50,
       disableBlurOnR: false,
-      landscape: false,
       loading: false,
+      allLoaded: false,
       confirmDialog: false,
       illustDeleteReady: null,
-      exporting: false,
-      importing: false,
-      pages: 0,
-      importTotal: 0,
-      importedCount: 0,
       searchQuery: '',
       searchTimeout: null,
       enableSaveVisitHistory: true
@@ -178,7 +144,6 @@ export default {
   beforeMount() {
     let vm = this;
 
-    this.loading = true;
     this.illustHistory = new IllustHistory();
 
     /**
@@ -186,25 +151,41 @@ export default {
      */
     this.enableSaveVisitHistory = this.browserItems.enableSaveVisitHistory;
 
+    /**
+     * Get general information
+     */
     this.illustHistory.init().then(() => {
       vm.illustHistory.db.allDocs({
         endkey: '_'
       }).then(items => {
-        vm.total = items.rows.length
-        vm.pages = Math.floor(vm.total / vm.step) + 1
+        vm.total = items.rows.length;
       })
+    });
+  },
 
-      vm.page = 1
-    })
+  mounted() {
+    let vm = this;
+
+    this.getIllusts()
+      .then(illusts => {
+        this.illusts = this.illusts.concat(illusts);
+        window.addEventListener('scroll', this.handleScroll);
+      });
+  },
+
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.handleScroll);
   },
 
   computed: {
-    imageAspectRatio() {
-      if (this.landscape) {
-        return 1.5
-      } else {
-        return 0.85
+    statusNotice() {
+      if (this.illusts.length <= 0) {
+        return 'There is no any history';
+      } else if (this.allLoaded) {
+        return 'There is no more history';
       }
+
+      return '';
     },
 
     importProgress() {
@@ -217,31 +198,29 @@ export default {
   },
 
   watch: {
-    page(val) {
-      if (this.searchQuery.trim().length === 0) {
-        this.getIllusts();
-      } else {
-        this.searchIllusts();
-      }
-    },
-
     searchQuery(val) {
       if (this.searchTimeout) {
         clearTimeout(this.searchTimeout);
       }
 
       this.searchTimeout = setTimeout(() => {
-        if (this.page == 1) {
-          if (val.trim().length === 0) {
-            this.getIllusts();
-          } else {
-            this.searchIllusts();
-          }
+        let promise;
 
-          return;
+        this.illusts = [];
+        this.offset = 0;
+
+        if (val.trim().length > 0) {
+          promise = this.searchIllusts();
+        } else {
+          promise = this.getIllusts();
         }
 
-        this.page = 1;
+        promise
+          .then(illusts => {
+            if (illusts.length > 0) {
+              this.illusts = this.illusts.concat(illusts);
+            }
+          })
       }, 800);
     }
   },
@@ -250,11 +229,15 @@ export default {
     caseDate(time) {
       let date = new Date(time * 1000);
 
-      return [date.getFullYear(), date.getMonth() + 1, date.getDate()].join('/');
+      if (browser.i18n.getUILanguage().toLowerCase().indexOf('zh') === 0) {
+        return [date.getFullYear(), date.getMonth() + 1, date.getDate()].join('/');
+      } else {
+        return [date.getMonth() + 1, date.getDate(), date.getFullYear()].join('/');
+      }
     },
 
     caseWorkUrl(id) {
-      return `https://www.pixiv.net/member_illust.php?mode=medium&illust_id=${id}`;
+      return `https://www.pixiv.net/artworks/${id}`;
     },
 
     caseWorkType(type) {
@@ -266,6 +249,28 @@ export default {
         return "Illustration";
       } else {
         return "Unkown";
+      }
+    },
+
+    handleScroll() {
+      if (document.documentElement.scrollHeight - window.innerHeight - window.scrollY < (window.innerHeight / 2)
+        && !this.loading
+        && !this.allLoaded
+      ) {
+        let promise;
+
+        if (this.searchQuery.trim().length > 0) {
+          promise = this.searchIllusts();
+        } else {
+          promise = this.getIllusts();
+        }
+
+        promise
+          .then(illusts => {
+            if (illusts.length > 0) {
+              this.illusts = this.illusts.concat(illusts);
+            }
+          });
       }
     },
 
@@ -297,22 +302,25 @@ export default {
      * Get illust histories
      */
     getIllusts() {
-      this.loading = true
+      return new Promise((resolve, reject) => {
+        this.loading = true;
 
-      this.illustHistory.getIllusts({
-        limit: this.step,
-        skip: (this.page - 1) * this.step
-      }).then(illusts => {
-        console.table(illusts);
-        this.loading = false
+        this.illustHistory.getIllusts({
+          limit: this.step,
+          skip: this.offset
+        }).then(illusts => {
+          this.offset += this.step;
 
-        if (!illusts || illusts.length < 1) {
-          return
-        }
+          if (illusts.length < this.step) {
+            this.allLoaded = true;
+          }
 
-        this.illusts = illusts
-      }).catch(err => {
-        console.log(err);
+          resolve(illusts);
+        }).catch(err => {
+          console.log(err);
+        }).finally(() => {
+          this.loading = false;
+        });;
       });
     },
 
@@ -322,190 +330,31 @@ export default {
     searchIllusts() {
       let vm = this;
 
-      this.illustHistory.searchIllusts({
-        limit: this.step,
-        skip: (this.page - 1) * this.step,
-        fun(doc, emit) {
-          if (doc.title.indexOf(vm.searchQuery) > -1) {
-            emit(doc);
-          }
-        }
-      }).then(illusts => {
-        this.loading = false;
-
-        if (!illusts || illusts.length < 1) {
-          this.illusts = [];
-        } else {
-          this.illusts = illusts;
-        }
-      }).catch(err => {
-        console.log(err);
-      });
-    },
-
-    changePage(page) {
-      this.page = page
-    },
-
-    prev() {
-      if (this.page <= 1) {
-        alert('There is no newer')
-        return
-      }
-
-      this.page--
-    },
-
-    next() {
-      if (this.page >= this.pages) {
-        alert('There is no older')
-        return
-      }
-
-      this.page++
-    },
-
-    insertData() {
-      let startId = 70114228
-      let endId = startId + 10000
-
-      this.putExampleData(startId, endId).then(() => {
-        console.log('complete')
-      }).catch(() => {
-        console.log('error')
-      })
-    },
-
-    putExampleData(startId, endId) {
-      let vm = this
-
-      let root = 'https://www.pixiv.net/ajax/illust/'
-
-      let url = root + startId
-
-      console.log('request ' + url)
-
       return new Promise(resolve => {
-        let xhr = new XMLHttpRequest()
-        xhr.open('get', url)
-        xhr.addEventListener('loadend', () => {
-          let data
+        this.loading = true;
 
-          try {
-            data = JSON.parse(xhr.responseText)
-
-            if (data.error === false && data.body) {
-              vm.illustHistory.putIllust({
-                id: data.body.illustId,
-                title: data.body.illustTitle,
-                images: data.body.urls,
-                type: data.body.illustType,
-                viewed_at: Math.floor(Date.now()),
-                r: data.body.xRestrict === 0 ? false : true
-              })
+        this.illustHistory.searchIllusts({
+          limit: this.step,
+          skip: this.offset,
+          fun(doc, emit) {
+            if (doc.title.toLowerCase().indexOf(vm.searchQuery.toLowerCase()) > -1) {
+              emit(doc);
             }
-          } catch (e) {
-            //do nothing
+          }
+        }).then(illusts => {
+          if (illusts.length < this.step) {
+            this.allLoaded = true;
           }
 
-          startId++
+          this.offset += this.step;
 
-          if (startId > endId) {
-            resolve()
-          } else {
-            resolve(vm.putExampleData(startId, endId))
-          }
-        })
-        xhr.send()
-      })
-    },
-
-    exportIllustHistory() {
-      if (this.exporting || this.importing) {
-        return
-      }
-
-      this.exporting = true
-
-      let json = ''
-      let vm = this
-
-      this.illustHistory.getIllusts({
-        limit: null
-      }).then(docs => {
-        let blob = new Blob([JSON.stringify(docs)], {type: 'application/json'})
-
-        let a = document.createElement('a')
-        a.href = URL.createObjectURL(blob)
-        a.download = 'illust_histories-' + Date.now() + '.json'
-        document.body.appendChild(a);
-        a.click()
-        a.remove();
-        vm.exporting = false
-      })
-    },
-
-    importIllustHistory() {
-      if (this.exporting || this.importing) {
-        return
-      }
-
-      let input = document.createElement('input')
-      let vm = this
-
-      input.type = 'file'
-      input.addEventListener('change', (e) => {
-        const files = e.target.files
-
-        let fileReader = new FileReader()
-
-        vm.importing = true
-
-        fileReader.addEventListener('load', () => {
-          let items
-
-          try {
-            items = JSON.parse(fileReader.result)
-
-            vm.importTotal = items.length
-
-            let worker = new Worker('./import_illust_history_worker.js')
-
-            worker.onmessage = e => {
-              vm.importedCount = e.data.importedCount
-
-              if (e.data.imported) {
-                vm.importedCount = items.length
-
-                vm.importTotal = 0 // disable displaying import progress
-
-                vm.importing = false
-
-                alert('Import complete, please reload the page.')
-              }
-            }
-
-            worker.postMessage({
-              items: items
-            })
-
-            // vm.importTotal = items.length
-
-            // vm.importIllustItems(items).then(() => {
-            //   alert('Import complete')
-            //   window.location.reload()
-            // })
-          } catch (e) {
-            console.log(e)
-          }
-        })
-
-        fileReader.readAsText(files[0])
-      })
-
-      if (window.confirm('Import history data will take a long time, after import completed, it will take a long time to rebuild indexes (based on the data size). The existing data will not be overwritten. Are you sure? ')) {
-        input.click()
-      }
+          resolve(illusts);
+        }).catch(err => {
+          console.log(err);
+        }).finally(() => {
+          this.loading = false;
+        });
+      });
     }
   }
 };
@@ -533,10 +382,14 @@ export default {
 .search-panel {
   margin: 10px 0;
 }
+</style>
 
+<style lang="scss">
 .illust-history-wrap {
   position: relative;
   margin-top: 20px;
+  box-shadow: 0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12);
+  background: #fff;
 
   .scroller {
     width: 100%;
@@ -550,18 +403,38 @@ export default {
     height: 90px;
     border-bottom: 1px solid #eee;
     background: #fff;
+
+    &:hover {
+      background: #f6f6f6;
+
+      .illust-content__actions {
+        width: 120px;
+      }
+    }
   }
 
   .illust-content__thumb-content {
     position: relative;
     width: 120px;
     height: 90px;
+    cursor: pointer;
+    overflow: hidden;
+    background: url(../assets/mask.gif) repeat;
   }
 
   .illust-content__thumb {
     position: absolute;
     width: 100%;
     height: 100%;
+    transition: opacity 0.5s;
+  }
+
+  .illust-content__thumb--blur {
+    opacity: 0;
+
+    &:hover {
+      opacity: 1;
+    }
   }
 
   .illust-content__info {
@@ -574,10 +447,23 @@ export default {
     white-space: nowrap;
     text-overflow: ellipsis;
     overflow: hidden;
+
+    a {
+      text-decoration: none;
+      color: #999;
+
+      &:hover {
+        color: #333;
+      }
+    }
   }
 
   .illust-content__info-entity--blod {
     font-weight: 700;
+  }
+
+  .illust-content__info-entity--sub {
+    color: #999;
   }
 
   .illust-content__badge {
@@ -604,13 +490,43 @@ export default {
   }
 
   .illust-content__actions {
-    width: 120px;
+    width: 0;
     text-align: right;
+    overflow: hidden;
   }
 
   .illust-content__actions-btn {
     margin: 27px 10px;
     color: rgb(88, 88, 88);
   }
+}
+
+.visit-history__header {
+  position: -webkit-sticky;
+  position: sticky;
+  top: 80px;
+  z-index: 9;
+
+  .v-input__slot {
+    box-shadow: 0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12);
+  }
+
+  .v-text-field__details {
+    display: none;
+  }
+}
+
+.visit-history__status-notice {
+  font-size: 14px;
+  text-align: center;
+  line-height: 4;
+  color: #ababab;
+  text-shadow: 0 -1px 0 #ccc;
+}
+
+.visit-content__menu {
+  position: absolute;
+  background: #fff;
+  box-shadow: 0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12);
 }
 </style>
