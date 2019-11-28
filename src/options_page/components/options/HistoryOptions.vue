b107369ee828831edcbcad38bdc97f4c0ec11e26
leoding86 <leoding86@gmail.com>
2019-11-28 10:04:14 +0800

    move export/import history data func to options

--- src/options_page/components/options/HistoryOptions.vue
+++ src/options_page/components/options/HistoryOptions.vue
@@ -25,11 +25,38 @@
 
         <v-list-tile>
           <v-list-tile-content>
+            <v-list-tile-title>{{ tl('Export_visit_history') }}</v-list-tile-title>
+          </v-list-tile-content>
+          <v-list-tile-action>
+            <v-btn
+              depressed
+              @click="exportVisitHistory"
+            >{{ tl('export') }}</v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+
+        <v-list-tile>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ tl('Import_visit_history') }}</v-list-tile-title>
+          </v-list-tile-content>
+          <v-list-tile-action>
+            <v-btn
+              depressed
+              @click="importVisitHistory"
+            >{{ tl('import') }}<span v-if="importTotal > 0"> ({{ importedCount }} / {{ importTotal }})</span></v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+
+        <v-list-tile>
+          <v-list-tile-content>
             <v-list-tile-title>{{ tl('clear_history_data') }}</v-list-tile-title>
             <v-list-tile-sub-title>{{ tl('clear_history_data_cannot_be_reversed') }}</v-list-tile-sub-title>
           </v-list-tile-content>
           <v-list-tile-action>
-            <v-btn @click="confirmDialog = true">{{ tl('Clear') }}</v-btn>
+            <v-btn
+              depressed
+              @click="confirmDialog = true"
+            >{{ tl('Clear') }}</v-btn>
           </v-list-tile-action>
         </v-list-tile>
       </v-list>
@@ -62,7 +89,11 @@ export default {
     return {
       enableSaveVisitHistory: true,
       notSaveNSFWWorkInHistory: false,
-      confirmDialog: false
+      confirmDialog: false,
+      importing: false,
+      exporting: false,
+      importTotal: 0,
+      importedCount: 0
     };
   },
 
@@ -91,6 +122,96 @@ export default {
     clearHistory() {
       this.illustHistory.clearData();
       this.confirmDialog = false;
+    },
+
+    exportVisitHistory() {
+      if (this.exporting || this.importing) {
+        return
+      }
+
+      this.exporting = true
+
+      let json = ''
+      let vm = this
+
+      this.illustHistory.getIllusts({
+        limit: null
+      }).then(docs => {
+        let blob = new Blob([JSON.stringify(docs)], {type: 'application/json'})
+
+        let a = document.createElement('a')
+        a.href = URL.createObjectURL(blob)
+        a.download = 'illust_histories-' + Date.now() + '.json'
+        document.body.appendChild(a);
+        a.click()
+        a.remove();
+        vm.exporting = false
+      })
+    },
+
+    importVisitHistory() {
+      if (this.exporting || this.importing) {
+        return
+      }
+
+      let input = document.createElement('input')
+      let vm = this
+
+      input.type = 'file'
+      input.addEventListener('change', (e) => {
+        const files = e.target.files
+
+        let fileReader = new FileReader()
+
+        vm.importing = true
+
+        fileReader.addEventListener('load', () => {
+          let items
+
+          try {
+            items = JSON.parse(fileReader.result)
+
+            vm.importTotal = items.length
+
+            let worker = new Worker('./import_illust_history_worker.js')
+
+            worker.onmessage = e => {
+              vm.importedCount = e.data.importedCount
+
+              if (e.data.imported) {
+                vm.importedCount = items.length
+
+                vm.importTotal = 0 // disable displaying import progress
+
+
+                setImmediate(() => {
+                  vm.importing = false
+                  alert('Import complete.')
+                });
+              }
+            }
+
+            worker.postMessage({
+              items: items
+            })
+
+            // vm.importTotal = items.length
+
+            // vm.importIllustItems(items).then(() => {
+            //   alert('Import complete')
+            //   window.location.reload()
+            // })
+          } catch (e) {
+            console.log(e)
+          }
+        })
+
+        fileReader.readAsText(files[0])
+      })
+
+      if (window.confirm('Import history data will take a long time, after import completed, it will take a long time to rebuild indexes (based on the data size). The existing data will not be overwritten. Are you sure? ')) {
+        input.click()
+      }
     }
   }
 };
