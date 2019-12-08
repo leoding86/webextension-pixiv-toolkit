ec1c3d55c2dc9457cc8b7f36bdb4cb65756e76dd
leoding86 <leoding86@gmail.com>
2019-12-08 21:27:06 +0800

    update active handler

--- src/content_scripts/components/App.vue
+++ src/content_scripts/components/App.vue
@@ -7,17 +7,38 @@
     <subscription-button class="ptk__handler"
       style="right:180px;"
       v-if="showSubscribe"
-      :user-id="userId"></subscription-button>
+      :user-id="userId"
+    ></subscription-button>
 
     <div class="ptk__container__body"
-      :class="{'ptk__container__body--show': showContainer}">
+      :class="{'ptk__container__body--show': showContainer}"
+    >
       <div class="ptk__container__body-container"
-        :class="{'ptk__container__body-container--show': showContainer}">
+        :class="{'ptk__container__body-container--show': showContainer}"
+      >
+        <div id="ptk__new-handler__wrapper">
+          <div id="ptk__new-handler"
+            @click="handlerClickHandle"
+          >
+            <svg viewBox="0 0 222 40" id="ptk__new-handler-bg" width="222" height="40" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">
+              <path d="M0 40 C 25 40, 43 27, 65 15 C 82 4, 93 0, 111 0, C 131 0, 142 4, 161 15, C 180 27, 198 40, 222 40, Z" stroke-width="0" fill="#0096fa"/>
+            </svg>
+            <svg viewBox="0 0 40 15" id="ptk__new-handler-arrow" width="40" height="25" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">
+              <path d="M 0 15, L 20 0, L 40 15" stroke="#ffffff" stroke-width="6" fill="transparent"/>
+              <circle cx="0" cy="15" r="3" stroke-width="0" fill="#ffffff"/>
+              <circle cx="40" cy="15" r="3" stroke-width="0" fill="#ffffff"/>
+            </svg>
+          </div>
+        </div>
         <ugoira-tool v-if="isUgoira" :tool="tool" class="ptk-tool__component">ugoira</ugoira-tool>
         <manga-tool v-else-if="isManga" :tool="tool" class="ptk-tool__component">manga</manga-tool>
         <illust-tool v-else-if="isIllust" :tool="tool" class="ptk-tool__component">illust</illust-tool>
         <novel-tool v-else-if="isNovel" :tool="tool" class="ptk-tool__component">novel</novel-tool>
         <ptk-button v-else text="Unsupported page"></ptk-button>
+        <div class="ptk-pixiv-omina-content" v-if="isUgoira || isManga || isIllust">
+          <ptk-button @click="passToPixivOmina"
+            :title="tl('_you_need_to_download_Pixiv_Omina_for_the_button_to_work')">Pixiv Omina</ptk-button>
+        </div>
       </div>
     </div>
   </div>
@@ -185,6 +206,10 @@ export default {
       browser.storage.local.set({
         featureKnown: true
       })
+    },
+
+    passToPixivOmina() {
+      window.location.assign(`pixiv-omina://create-download?url=${encodeURIComponent(window.location.href)}`);
     }
   }
 };
@@ -202,22 +227,26 @@ export default {
 
   .ptk__container__body {
     position: absolute;
+    width: 100%;
     overflow: visible;
-    display: inline-block;
-    right: 50%;
+    // display: inline-block;
+    // right: 50%;
+    text-align: center;
     transition: all 0.5s;
 
     .ptk__container__body-container {
       display: inline-block;
       position: relative;
       top: 0;
-      left: 50%;
-      padding: 8px 12px;
+      // left: 50%;
+      // padding: 8px 12px;
       background: #fff;
       border-radius: 30px;
       box-shadow: 0 0 8px rgba(0,0,0,0.3);
       overflow: hidden;
       transition: all 0.5s;
+      box-sizing: border-box;
+      overflow: visible;
 
       .button {
         &:last-child {
@@ -228,6 +257,15 @@ export default {
 
     .ptk__container__body-container--show {
       top: -50px;
+
+      #ptk__new-handler-arrow {
+        transform: rotate(180deg);
+      }
+
+      #ptk__new-handler__wrapper {
+        top: -18px;
+        height: 18px;
+      }
     }
   }
 
@@ -254,8 +292,56 @@ export default {
     box-shadow: 0px -2px 5px rgba(0,0,0,0.3);
   }
 
+  #ptk__new-handler__wrapper {
+    position: absolute;
+    top: -15px;
+    left: 0;
+    width: 100%;
+    height: 15px;
+    transition: all 300ms;
+    overflow: hidden;
+
+    &:hover {
+      top: -18px;
+      height: 18px;
+    }
+  }
+
+  #ptk__new-handler {
+    position: relative;
+    margin: 0 auto;
+    width: 100px;
+    height: 18px;
+    cursor: pointer;
+  }
+
+  #ptk__new-handler-bg {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+  }
+
+  #ptk__new-handler-arrow {
+      width: 13px;
+      height: 8px;
+      position: absolute;
+      top: 5px;
+      left: 44px;
+      transition: all 500ms;
+  }
+
   .ptk-tool__component {
+    position: relative;
     display: inline-block;
+    padding: 8px 12px;
+  }
+
+  .ptk-pixiv-omina-content {
+    display: inline-block;
+    padding-right: 12px;
+    // border-left: 2px solid #dcdcdc;
   }
 }
 </style>
