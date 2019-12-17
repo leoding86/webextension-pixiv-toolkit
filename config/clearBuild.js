5467b375cc3ccd397c1b58a08df4a3b1bb1931ce
leoding86 <leoding86@gmail.com>
2019-12-17 10:26:52 +0800

    update webpack config

--- config/clearBuild.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs-extra');
-
-// clean dist
-module.exports = env => {
-    let platform = env ? (env.platform || 'chrome') : 'chrome';
-    fs.emptyDirSync(`./dist/${platform}`)
-  }
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 'use strict'
 
+const fs = require('fs-extra');
 const commonConfig = require('./config/webpack.common.config');
 const backgroundConfig = require('./config/webpack.background.config');
 const contentScriptsConfig = require('./config/webpack.content-scripts.config');
 const optionsPageConfig = require('./config/webpack.options-page.config');
 const popupConfig = require('./config/webpack.popup.config');
 
-require('./config/clearBuild')();
+const platform = process.env.PLATFORM_ENV === 'firefox' ?
+  'firefox' : 'chrome';
+
+fs.emptyDirSync(`./dist/${platform}`)
 
 module.exports = [
   commonConfig,
