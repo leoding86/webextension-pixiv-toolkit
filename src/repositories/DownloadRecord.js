03534d27310a127f0d70c2a52c7f0a63accdf018
Leo Ding <leoding86@gmail.com>
2020-02-22 16:38:58 +0800

    Add DownloadRecord.js

---
+++ src/repositories/DownloadRecord.js
@@ -0,0 +1,77 @@
+import PouchDB from 'pouchdb';
+import PouchDBFind from 'pouchdb-find';
+
+PouchDB.plugin(PouchDBFind)
+
+class DownloadRecord {
+  static instance;
+
+  constructor() {
+    this.db = new PouchDB('download_record', {
+      revs_limit: 1
+    });
+
+    this.maxLimit = 5000;
+
+    this.init();
+  }
+
+  /**
+   * @returns {DownloadRecord}
+   */
+  static getDefault() {
+    if (!DownloadRecord.instance) {
+      DownloadRecord.instance = new DownloadRecord();
+    }
+
+    return DownloadRecord.instance;
+  }
+
+  init() {
+    return this.db.createIndex({
+      index: {
+        fields: ['downloaded_at']
+      }
+    })
+  }
+
+  checkLimitationAndRemove() {
+    this.db.allDocs().then(entries => {
+      let deleteLength = entries.rows.length - this.maxLimit;
+
+      if (deleteLength > 0) {
+        this.db.find({
+          selector: { downloaded_at: { $gte: null } },
+          sort: [{ 'downloaded_at': "desc"}],
+          limit: deleteLength
+        }).then(response => {
+          response.docs.forEach(doc => {
+            this.db.remove(doc).catch(err => {
+              console.log(err);
+            });
+          })
+        })
+      }
+    })
+  }
+
+  putRecord(record) {
+    this.checkLimitationAndRemove();
+
+    return this.retrieveRecord(record._id).then(doc => {
+      doc = Object.assign(doc, record);
+
+      return this.db.put(doc);
+    }).catch(error => {
+      if (error.status === 404) {
+        return this.db.put(record);
+      }
+    });
+  }
+
+  retrieveRecord(id) {
+    return this.db.get(id);
+  }
+}
+
+export default DownloadRecord;
