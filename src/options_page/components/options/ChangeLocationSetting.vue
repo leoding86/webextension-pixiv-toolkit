f4c0ff52759991edf00c993a1804d79bc79f5bb9
Leo Ding <leoding86@gmail.com>
2020-03-02 22:42:17 +0800

    add ChangeLocationSetting.vue

---
+++ src/options_page/components/options/ChangeLocationSetting.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-list-tile>
+    <v-list-tile-content>
+      <v-list-tile-title>
+        {{ settingTitle }}
+        <v-tooltip
+          v-if="settingTip !== ''"
+          bottom
+        >
+          <template
+            v-slot:activator="{ on }"
+          >
+            <v-icon
+              v-on="on"
+              small
+            >info</v-icon>
+          </template>
+          <span>{{ settingTip }}</span>
+        </v-tooltip>
+      </v-list-tile-title>
+      <v-list-tile-sub-title>{{ settingHint }}</v-list-tile-sub-title>
+    </v-list-tile-content>
+    <v-list-tile-action>
+      <change-location-btn
+        :dialog-title="dialogTitle"
+        :dialog-hint="dialogHint"
+        :location.sync="location"
+        :disabled="!browserItems.enableExtTakeOverDownloads"
+      ></change-location-btn>
+    </v-list-tile-action>
+  </v-list-tile>
+</template>
+
+<script>
+import SuperMixin from "@/mixins/SuperMixin";
+import ChangeLocationBtn from "@@/components/options/ChangeLocationBtn";
+
+export default {
+  mixins: [SuperMixin],
+
+  components: {
+    'change-location-btn': ChangeLocationBtn
+  },
+
+  props: {
+    value: {
+      required: true,
+      type: String
+    },
+
+    settingTitle: {
+      required: true,
+      type: String
+    },
+
+    settingTip: {
+      required: false,
+      type: String,
+      default: ''
+    },
+
+    dialogTitle: {
+      required: true,
+      type: String
+    },
+
+    dialogHint: {
+      required: true,
+      type: String
+    },
+  },
+
+  computed: {
+    settingHint() {
+      if (this.location) {
+        return this.location;
+      } else {
+        return 'Not set';
+      }
+    },
+
+    location: {
+      get() {
+        return this.value;
+      },
+
+      set(val) {
+        this.$emit('input', val);
+      }
+    }
+  }
+}
+</script>
