b05f3cc1bcc2e44494670da25fb088f26bbaa578
Leo Ding <leoding86@gmail.com>
2020-03-03 16:16:26 +0800

    update downloadFileMixin.js

--- src/content_scripts/mixins/downloadFileMixin.js
+++ src/content_scripts/mixins/downloadFileMixin.js
@@ -3,24 +3,59 @@ import {
   downloads as browserDownloads
 } from '@/content_scripts/Browser';
 import browser from '@/modules/Extension/browser';
+import formatName from "@/modules/Util/formatName";
 
 export default {
   methods: {
-    downloadFile(url, filename, extra) {
+    downloadFile(url, filename, { folder = null, statType = null }) {
       let vm = this;
+      let blob = null;
+
+      if (url instanceof Blob) {
+        blob = url;
+        url = URL.createObjectURL(url);
+      }
 
       /**
        * Because some sercurity reason of Firefox, the background script cannot download blob url from other sites.
        */
-      if (!this.isFirefox && this.browserItems.enableExtTakeOverDownloads) {
+      if (this.browserItems.enableExtTakeOverDownloads) {
         browserPermissions.contains({
           permissions: ['downloads']
         }).then(result => {
           if (result) {
-            browserDownloads.download({
-              url: url,
-              filename: vm.browserItems.downloadRelativeLocation ? vm.browserItems.downloadRelativeLocation : '' + filename
-            });
+            let filepath = '';
+
+            if (folder) {
+              filepath += folder;
+            } else if (vm.browserItems.downloadRelativeLocation) {
+              filepath += vm.browserItems.downloadRelativeLocation;
+            }
+
+            filepath += filename;
+
+            if (this.isFirefox && blob) {
+              let fileReader = new FileReader();
+
+              fileReader.readAsArrayBuffer(blob);
+
+              fileReader.onload = event => {
+                browser.runtime.sendMessage({
+                  action: 'download',
+                  options: {
+                    arrayBuffer: fileReader.result,
+                    filename: filepath,
+                    saveAs: vm.browserItems.downloadSaveAs
+                  }
+                });
+              }
+            } else {
+              browserDownloads.download({
+                url: url,
+                filename: filepath,
+                saveAs: vm.browserItems.downloadSaveAs
+              });
+            }
           } else {
             alert('You need grant download permission for downloading using downlads setting in extension');
           }
@@ -39,12 +74,32 @@ export default {
         a.remove()
       }
 
-      if (extra && extra.statType) {
+      if (statType) {
         browser.runtime.sendMessage({
           action: 'updateDownloadedStat',
-          args: extra.statType
+          args: statType
         });
       }
+    },
+
+    getSubfolder(folderName, context) {
+      let folder = '';
+
+      if (folderName) {
+        let folders = folderName.split('/');
+
+        folders = folders.map(item => {
+          if (item === "") {
+            return "";
+          }
+
+          return formatName(item, context, item);
+        });
+
+        folder = folders.join('/');
+      }
+
+      return folder;
     }
   }
 }
