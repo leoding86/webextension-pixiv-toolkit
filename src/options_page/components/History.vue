1f46fa874eec33d3bfa1db95636851ce86fed050
Leo Ding <leoding86@gmail.com>
2020-03-03 16:12:20 +0800

    add ChangeLog component

--- src/options_page/components/History.vue
+++ src/options_page/components/History.vue
@@ -6,7 +6,7 @@
       <v-card-text>
         <img src="https://raw.githubusercontent.com/leoding86/webextension-pixiv-toolkit/master/src/statics/remote/img/example.gif"
           style="width:100%;border-radius: 5px;box-shadow:0 1px 3px rgba(0,0,0,0.5);margin-bottom:15px;">
-        <div style="font-size:14px" v-html="history"></div>
+        <change-log></change-log>
       </v-card-text>
     </v-card>
   </v-container>
@@ -14,46 +14,13 @@
 
 <script>
 import SuperMixin from '@/mixins/SuperMixin';
+import ChangeLog from '@/options_page/components/ChangeLog';
 
 export default {
-  mixins: [
-    SuperMixin
-  ],
+  mixins: [ SuperMixin ],
 
-  data () {
-    return {
-      xhr: null,
-      history: 'loading'
-    }
-  },
-
-  mounted () {
-    let vm = this;
-
-    this.loadHistory().then(history => {
-      vm.history = history;
-    });
-  },
-
-  methods: {
-    loadHistory () {
-      return new Promise(resolve => {
-        if (!this.xhr) {
-          this.xhr = new XMLHttpRequest();
-        }
-
-        this.xhr.open('get', browser.runtime.getURL('HISTORY'));
-
-        this.xhr.setRequestHeader("Content-Type", "text/plain");
-
-        this.xhr.onload = () => {
-          let parts = this.xhr.responseText.split(/-{46}/);
-          resolve(parts[1].trim().replace(/\r\n|\r|\n/g, '<br>'));
-        };
-
-        this.xhr.send();
-      });
-    }
+  components: {
+    'change-log': ChangeLog
   }
 }
 </script>
@@ -65,15 +32,5 @@ export default {
         font-size: 18px;
         margin-bottom: 10px;
     }
-}
-
-.history-entry {
-  font-size: 14px;
-  margin-bottom: 0;
-  padding-bottom: 15px;
-
-  strong {
-    font-size: 16px;
-  }
 }
 </style>
