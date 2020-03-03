5dbf1e98bdcac990ca4cd5ec9ff2e14b2302b552
Leo Ding <leoding86@gmail.com>
2020-03-03 16:15:00 +0800

    add errorCallback for PackageFileReader.read

--- src/modules/Util/PackageFileReader.js
+++ src/modules/Util/PackageFileReader.js
@@ -1,21 +1,21 @@
 import Browser from '@/modules/Browser/Browser';
 
-function PackageFileReader() {
-    //
-}
-
-PackageFileReader.read = function (path, callback) {
-    Browser.getBrowser().runtime.getPackageDirectoryEntry(function (directoryEntry) {
-        directoryEntry.getFile(path, undefined, function (fileEntry) {
-            fileEntry.file(function (file) {
-                var reader = new FileReader;
-                reader.addEventListener("load", function (event) {
-                    callback(reader.result);
-                });
-                reader.readAsText(file);
-            });
+class PackageFileReader {
+  static read(path, callback, errorCallback) {
+    Browser.getBrowser().runtime.getPackageDirectoryEntry(directoryEntry => {
+      directoryEntry.getFile(path, undefined, fileEntry => {
+        fileEntry.file(file => {
+          let reader = new FileReader;
+          reader.addEventListener("load", event => {
+            callback(reader.result);
+          });
+          reader.readAsText(file);
         });
+      }, error => {
+        errorCallback(error);
+      });
     });
+  }
 }
 
 export default PackageFileReader;
