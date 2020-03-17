ac5e3992b7f7c6b35ed5305b76508c4abf35e0d7
leoding86 <leoding86@gmail.com>
2020-03-17 17:26:23 +0800

    refactor Updater module

--- src/modules/Util/Updater.js
+++ src/modules/Util/Updater.js
@@ -1,49 +1,38 @@
 import Browser from '@/modules/Browser/Browser';
 import versionCompare from '@/modules/Util/versionCompare';
 
-function Updater(currentSettings) {
-    this.currentSettings = currentSettings,
+class Updater {
+  constructor(currentSettings, defaultSettings) {
+    this.currentSettings = currentSettings;
     this.browser = Browser.getBrowser();
-    this.defaultSettings;
-}
+    this.defaultSettings = defaultSettings;
+  }
 
-Updater.prototype = {
-    setDefaultSettings: function (defaultSettings) {
-        this.defaultSettings = defaultSettings;
-        return this;
-    },
+  isNewer(version) {
+      return !this.currentSettings.version || versionCompare(this.currentSettings.version, version) < 0
+  }
 
-    removeSettings: function (settingsNeededRemoved) {
-        this.browser.storage.local.remove(settingsNeededRemoved);
-    },
+  mergeSettings(overrideSettings = {}) {
+    Object.assign(this.currentSettings, overrideSettings);
+    let currentSettingKeys = Object.keys(this.currentSettings);
+    let defaultSettingKeys = Object.keys(this.defaultSettings);
 
-    mergeSettings: function (callback) {
-        var self = this;
+    defaultSettingKeys.forEach(key => {
+      if (undefined === this.currentSettings[key]) {
+        this.currentSettings[key] = defaultSettings[key];
+      }
+    });
 
-        Object.keys(this.defaultSettings).forEach(function (key) {
-            if (undefined === self.currentSettings[key]) {
-                self.currentSettings[key] = self.defaultSettings[key];
-            }
+    let removeSettingKeys = currentSettingKeys.filter(key => defaultSettingKeys.indexOf(key) === -1);
+
+    return new Promise(resolve => {
+      this.browser.storage.local.set(this.currentSettings, () => {
+        this.browser.storage.local.remove(removeSettingKeys, () => {
+          resolve();
         });
-
-        this.storageSettings(this.currentSettings, callback);
-    },
-
-    updateSetting: function (setting, callback) {
-        this.storageSettings(setting, callback);
-    },
-
-    storageSettings: function (settings, callback) {
-        let self = this;
-
-        this.browser.storage.local.set(settings, function () {
-            callback.call(self);
-        });
-    },
-
-    isNewer: function (version) {
-        return !this.currentSettings.version || versionCompare(this.currentSettings.version, version) < 0
-    }
+      });
+    });
+  }
 }
 
 export default Updater;
