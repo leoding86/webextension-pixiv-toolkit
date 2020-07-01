920d39c9dc94867fb050ba13ccbf24fadcaa71b1
leoding86 <leoding86@gmail.com>
2020-07-01 20:44:49 +0800

    add `Locales.js`

---
+++ src/modules/Locales.js
@@ -0,0 +1,7 @@
+import localeEn from '../statics/_locales/en/messages.json';
+import localeZhCN from '../statics/_locales/zh_CN/messages.json';
+
+export default {
+  localeEn: localeEn,
+  localeZhCN: localeZhCN
+};
