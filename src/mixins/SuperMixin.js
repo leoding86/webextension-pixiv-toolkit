5c5f3b355ec25ab4347aa5730de0ec77d88bdbab
leoding86 <leoding86@gmail.com>
2020-07-01 20:47:56 +0800

    migrate `pushRoute` method to `SuperMixin`

--- src/mixins/SuperMixin.js
+++ src/mixins/SuperMixin.js
@@ -20,6 +20,25 @@ export default {
       }
 
       return content.replace(/<\/?[^>]+(>|$)/g, replace.repeat(2)).trim();
+    },
+
+    routeTo (name) {
+      this.pushRoute({
+        name: name
+      })
+    },
+
+    pushRoute(args) {
+      return this.$router.push(args).then(() => {
+        return Promise.resolve();
+      }).catch(error => {
+        if (error.name === 'NavigationDuplicated') {
+          // ignore
+          return;
+        }
+
+        return Promise.reject(error);
+      });
     }
   }
 }
