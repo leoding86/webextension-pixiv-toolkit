38df640a03c69ee22d9975ae38ebec9f4a33425a
leoding86 <leoding86@gmail.com>
2020-08-25 18:28:49 +0800

    add methods
    add deleteDownload, listDownloads methods

--- src/repositories/DownloadRecord.js
+++ src/repositories/DownloadRecord.js
@@ -3,12 +3,12 @@ import PouchDB from 'pouchdb';
 class DownloadRecord {
   static instance;
 
-  constructor({ max = 10000 }) {
+  constructor(options = { max: 10000 }) {
     this.db = new PouchDB('download_record', {
       revs_limit: 1
     });
 
-    this.maxLimit = max;
+    this.maxLimit = options.max;
 
     this.init();
   }
@@ -56,7 +56,9 @@ class DownloadRecord {
     this.checkLimitationAndRemove();
 
     return this.retrieveRecord(record._id).then(doc => {
-      doc = Object.assign(doc, record);
+      doc = Object.assign(doc, record, {
+        downloaded_at: Math.floor(Date.now() / 1000)
+      });
 
       return this.db.put(doc);
     }).catch(error => {
@@ -64,6 +66,58 @@ class DownloadRecord {
         return this.db.put(record);
       }
     });
+  }
+
+  /**
+   * Get a download using download's id
+   * @param {string} id
+   */
+  getDownload(id) {
+    return this.db.get(id);
+  }
+
+  /**
+   * Delete a download using download's id
+   * @param {string} id
+   */
+  deleteDownload(id) {
+    return this.getDownload(id).then(doc => {
+      return this.db.remove(doc);
+    }).catch(error => {
+      throw error;
+    });
+  }
+
+  /**
+   * List downloads
+   * @param {Object} options
+   * @param {Object} [options.selector={downloaded_at:{$gte:null}}]
+   * @param {Object} [options.sort=[{downloaded_at:'desc'}]]
+   * @param {Number} [options.limit=50]
+   * @param {Number} [options.skip=0]
+   * @param {(Function|null)} [options.fun=null]
+   */
+  listDownloads(options) {
+    let queryOptions = {
+      selector: options.selector || { downloaded_at: { $gte: null } },
+      sort: options.sort || [{ downloaded_at: 'desc' }],
+      limit: options.limit || 50,
+      skip: options.skip || 0,
+    }, fun = options.fun || null;
+
+    if (!fun) {
+      return this.db.find(queryOptions).then(response => {
+        return response.docs;
+      });
+    } else {
+      queryOptions.include_docs = true;
+
+      return this.db.query(fun, queryOptions).then(response => {
+        return response.rows.map(row => {
+          return row.doc;
+        });
+      });
+    }
   }
 
   retrieveRecord(id) {
