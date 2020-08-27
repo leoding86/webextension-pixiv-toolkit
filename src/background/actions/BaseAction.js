24fe340606457239290798ebed83f82a4709f091
leoding86 <leoding86@gmail.com>
2020-08-27 17:58:25 +0800

    delete useless files

--- src/background/actions/BaseAction.js
+++
@@ -1,9 +0,0 @@
-import Browser from '@/modules/Browser/Browser'
-
-class BaseAction {
-  constructor() {
-    this.browser = Browser.getBrowser()
-  }
-}
-
-export default BaseAction
--- src/options_page/workers/global.js
+++
@@ -1,3 +0,0 @@
-if (typeof global === 'undefined') {
-  self.global = typeof window === 'undefined' ? self : window;
-}
