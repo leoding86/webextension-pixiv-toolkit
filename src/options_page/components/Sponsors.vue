e90c8a9da16f54d7794eb8382091532bc6eb9089
leoding86 <leoding86@gmail.com>
2021-07-13 16:40:24 +0800

    seperate sponsors list from sponsors page

--- src/options_page/components/Sponsors.vue
+++ src/options_page/components/Sponsors.vue
@@ -1,57 +1,17 @@
 <template>
   <v-container>
-    <v-card>
-      <v-card-text>
-        <p style="font-size:14px;">{{ tl('sponsor_update_periodically') }} ~ヾ(＾∇＾)</p>
-        <p style="font-size:14px;" v-if="sponsors === null">Loading sponsors...</p>
-        <ul v-else-if="sponsors.length > 0">
-          <li v-for="(sponsor, i) in sponsors" :key="i" :class="'sponsor sponsor__level-' + sponsor.level">
-            <span class="sponser__glitter-container"></span>
-            <span class="sponser__name">{{ sponsor.name }}</span>
-          </li>
-        </ul>
-        <p class="no-sponsor" v-else>{{ tl('sponsor_very_first') }} ~ヾ(＾∇＾)</p>
-      </v-card-text>
-    </v-card>
+    <sponsors-card></sponsors-card>
   </v-container>
 </template>
 
 <script>
 import cr from "@@/modules/cr";
-import Supports from '@@/components/Supports';
+import SponsorsCard from '@/options_page/components/SponsorsCard.vue';
 import sponsersData from '@@/../statics/sponsors.json';
 
 export default {
   components: {
-    supports: Supports
-  },
-
-  data () {
-    return {
-      sponsors: null
-    }
-  },
-
-  mounted () {
-    let vm = this,
-        sponsorsUrl = 'https://raw.githubusercontent.com/leoding86/webextension-pixiv-toolkit/master/src/statics/sponsors.json',
-        xhr = new XMLHttpRequest();
-
-    xhr.open('get', sponsorsUrl);
-
-    xhr.onload = function () {
-      let sponsors = JSON.parse(this.responseText);
-
-      vm.sponsors = (Object.prototype.toString.call(sponsors) === '[object Array]' && sponsors.length > 0) ? sponsors : [];
-    };
-
-    xhr.send();
-  },
-
-  methods: {
-    tl(string) {
-      return cr._e(string);
-    }
+    'sponsors-card': SponsorsCard
   }
 }
 </script>
---
+++ src/options_page/components/SponsorsCard.vue
@@ -0,0 +1,121 @@
+<template>
+  <v-card>
+    <v-card-text>
+      <p style="font-size:14px;">{{ tl('sponsor_update_periodically') }} ~ヾ(＾∇＾)</p>
+      <p style="font-size:14px;" v-if="sponsors === null">Loading sponsors...</p>
+      <ul v-else-if="sponsors.length > 0">
+        <li v-for="(sponsor, i) in sponsors" :key="i" :class="'sponsor sponsor__level-' + sponsor.level">
+          <span class="sponser__glitter-container"></span>
+          <span class="sponser__name">{{ sponsor.name }}</span>
+        </li>
+      </ul>
+      <p class="no-sponsor" v-else>{{ tl('sponsor_very_first') }} ~ヾ(＾∇＾)</p>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import cr from "@@/modules/cr";
+import Supports from '@@/components/Supports';
+import sponsersData from '@@/../statics/sponsors.json';
+
+export default {
+  components: {
+    supports: Supports
+  },
+
+  data () {
+    return {
+      sponsors: null
+    }
+  },
+
+  mounted () {
+    let vm = this,
+        sponsorsUrl = 'https://raw.githubusercontent.com/leoding86/webextension-pixiv-toolkit/master/src/statics/sponsors.json',
+        xhr = new XMLHttpRequest();
+
+    xhr.open('get', sponsorsUrl);
+
+    xhr.onload = function () {
+      let sponsors = JSON.parse(this.responseText);
+
+      vm.sponsors = (Object.prototype.toString.call(sponsors) === '[object Array]' && sponsors.length > 0) ? sponsors : [];
+    };
+
+    xhr.send();
+  },
+
+  methods: {
+    tl(string) {
+      return cr._e(string);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+p.no-sponsor {
+  font-size: 16px;
+  margin-bottom: 0;
+}
+
+ul {
+  list-style: none;
+  padding-left: 0;
+
+  li {
+    display: inline-block;
+    margin-bottom: 10px;
+    font-size: 16px;
+  }
+}
+
+$text-color: #ffed21;
+$text-shadow: 2px 2px 0 #df7c11;
+
+.sponsor {
+
+  display: inline-block;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  padding: 5px 15px;
+  margin-right: 10px;
+  border-radius: 100px;
+  background: #fff;
+  box-shadow: 3px 3px 0px #ccc;
+
+  .sponser__name {
+    position: relative;
+  }
+}
+
+.sponsor__level-1 {
+  font-weight: 300;
+}
+
+.sponsor__level-2 {
+  font-weight: 1000;
+  background: #585c56;
+  color: $text-color;
+  text-shadow: $text-shadow;
+}
+
+.sponsor__level-3 {
+  position: relative;
+  font-weight: 1000;
+  background: #585c56;
+  color: $text-color;
+  text-shadow: $text-shadow;
+
+  .sponser__glitter-container {
+    display: block;
+    position: absolute;
+    top: -20%;
+    left: -10%;
+    width: 120%;
+    height: 140%;
+    background: url(../assets/glitter.gif) 0 -10px;
+  }
+}
+</style>
