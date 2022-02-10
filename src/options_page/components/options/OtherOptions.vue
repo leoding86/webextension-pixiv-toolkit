4f2fa6d3047b8c67a16fbef2d6fd14c2847eaf54
leoding86 <leoding86@gmail.com>
2022-02-10 21:13:15 +0800

    move some options to interface setting section

--- src/options_page/components/options/OtherOptions.vue
+++ src/options_page/components/options/OtherOptions.vue
@@ -6,36 +6,6 @@
       <v-list two-line>
         <v-list-tile>
           <v-list-tile-content>
-            <v-list-tile-title>{{ tl('_language') }}</v-list-tile-title>
-          </v-list-tile-content>
-          <v-list-tile-action>
-            <v-select
-              :items="languageOptions"
-              v-model="language"
-              type="value"
-              @change="onLanguageChangeHandler"
-              style="width:150px"
-            >
-            </v-select>
-          </v-list-tile-action>
-        </v-list-tile>
-      </v-list>
-
-      <v-list two-line>
-        <v-list-tile>
-          <v-list-tile-content>
-            <v-list-tile-title>{{ tl('Activate_download_panel_automatically') }}</v-list-tile-title>
-            <v-list-tile-sub-title>{{ tl('Download_panel_will_show_up_automatically_when_page_loaded') }}</v-list-tile-sub-title>
-          </v-list-tile-content>
-          <v-list-tile-action>
-            <v-switch v-model="autoActivateDownloadPanel"></v-switch>
-          </v-list-tile-action>
-        </v-list-tile>
-      </v-list>
-
-      <v-list two-line>
-        <v-list-tile>
-          <v-list-tile-content>
             <v-list-tile-title>{{ tl('_export_settings') }}</v-list-tile-title>
           </v-list-tile-content>
           <v-list-tile-action>
@@ -127,51 +97,7 @@
 import defaultSettings from '@/config/default';
 
 export default {
-  data() {
-    return {
-      autoActivateDownloadPanel: false,
-
-      language: 'en',
-    };
-  },
-
-  computed: {
-    languageOptions() {
-      return [
-        {
-          text: this.tl('_default'),
-          value: 'default'
-        }, {
-          text: '简体中文',
-          value: 'zh_CN',
-        }, {
-          text: 'English',
-          value: 'en'
-        }
-      ];
-    }
-  },
-
-  beforeMount() {
-    this.autoActivateDownloadPanel = this.browserItems.autoActivateDownloadPanel;
-    this.language = this.browserItems.language || 'default';
-  },
-
-  watch: {
-    autoActivateDownloadPanel(val) {
-      browser.storage.local.set({
-        autoActivateDownloadPanel: !!val
-      });
-    }
-  },
-
   methods: {
-    onLanguageChangeHandler(val) {
-      browser.storage.local.set({
-        language: val
-      });
-    },
-
     exportSettings({ excludeHistoryBackup = false }) {
       let settings = Object.assign({}, this.browserItems);
 
