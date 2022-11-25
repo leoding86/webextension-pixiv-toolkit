dee8adcc78509da4b2bb0ddac902b6990abb497f
leoding86 <leoding86@gmail.com>
2022-11-25 20:46:34 +0800

    replace history service
    replace DownloadHistoryService and VisitHistoryService by HistoryService

--- src/options_page/services/index.js
+++ src/options_page/services/index.js
@@ -1,19 +1,17 @@
 import BadgeService from "./BadgeService";
-import DownloadHistoryService from "./DownloadHistoryService";
 import DownloadService from "./DownloadService";
+import HistoryService from "./HistoryService";
 import LogService from "./LogService";
 import PermissionService from "./PermissionService";
 import SettingService from "./SettingService";
 import UtilService from "./UtilService";
-import VisitHistoryService from "./VisitHistoryService";
 
 export default {
   BadgeService,
-  DownloadHistoryService,
   DownloadService,
+  HistoryService,
   LogService,
   PermissionService,
   SettingService,
   UtilService,
-  VisitHistoryService,
 };
