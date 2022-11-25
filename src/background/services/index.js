03e8bb40bc08cf3c4b5a3197d605939e2dba8030
leoding86 <leoding86@gmail.com>
2022-11-25 20:45:08 +0800

    delete DownloadHistoryService

--- src/background/services/index.js
+++ src/background/services/index.js
@@ -1,13 +1,9 @@
 import BadgeService from "./BadgeService";
-import DownloadHistoryService from "./DownloadHistoryService";
 import LogService from "./LogService";
 import SettingService from "./SettingService";
-import WebRequestService from "./WebRequestService";
 
 export default {
   BadgeService,
-  DownloadHistoryService,
   LogService,
   SettingService,
-  // WebRequestService,
 };
