5742e7c18ef1823daa32cf6afb8dd8eb5e625bf2
leoding86 <leoding86@gmail.com>
2024-08-11 17:14:26 +0800

    add checkReady method for checking if download manager is ready

--- src/options_page/services/DownloadService.js
+++ src/options_page/services/DownloadService.js
@@ -35,6 +35,10 @@ class DownloadService extends AbstractService {
     this.downloadManager.setMaxDownloadingTasks(this.application.settings.maxProcessDownloadTasks);
   }
 
+  checkReady() {
+    return this.downloadManager ? true :  false;
+  }
+
   /**
    * @deprecated
    * @param {any} param0
@@ -171,7 +175,7 @@ class DownloadService extends AbstractService {
 
   async checkIfDownloadManagerOpened() {
     let tab = await browser.tabs.getCurrent();
-console.log(tab);
+
     return {
       result: window.__CURRENT_ACTIVE_DM__,
       data: {
