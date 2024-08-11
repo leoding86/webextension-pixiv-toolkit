f5078ecd3fd1660e7c4e4763511f5635e16d03d8
leoding86 <leoding86@gmail.com>
2024-08-11 17:22:52 +0800

    update

--- src/content_scripts/modules/ContentPageDownloadManager.js
+++ src/content_scripts/modules/ContentPageDownloadManager.js
@@ -1,7 +1,16 @@
+/*
+ * @Author: Leo Ding <leoding86@msn.com>
+ * @Date: 2024-08-11 10:45:18
+ * @LastEditors: Leo Ding <leoding86@msn.com>
+ * @LastEditTime: 2024-08-11 16:12:26
+ * @FilePath: \webextension-pixiv-toolkit\src\content_scripts\modules\ContentPageDownloadManager.js
+ */
 import DownloadManager from "@/options_page/modules/DownloadManager";
 import DownloadAdapter from "@/options_page/modules/DownloadAdapter";
 import Event from '@/modules/Event';
 import AbstractResource from "@/modules/PageResource/AbstractResource";
+import GifGenerator from "./Legacy/UgoiraGenerator/GifGenerator";
+import { RuntimeError } from "@/errors";
 
 class ContentPageDownloadManager extends Event {
   /**
@@ -25,8 +34,9 @@ class ContentPageDownloadManager extends Event {
         instance.dispatch('error', [error]);
       });
 
-      instance.downloadManager.addListener('update', (status) => {
-        instance.dispatch('update', [status]);
+      instance.downloadManager.addListener('update', (taskId) => {
+        const downloadTask = instance.downloadManager.getTask(taskId);
+        instance.dispatch('update', [downloadTask]);
       });
     }
 
@@ -36,16 +46,24 @@ class ContentPageDownloadManager extends Event {
   /**
    * @param {AbstractResource} resource
    * @param {{ ugoiraConvertType: string|undefined, selectedIndexes: number[]|undefined }} options
+   * @param {{ redownload: boolean|undefined }} downloadOptions
    */
-  async addTask(resource, options) {
-    try {
-      const downloadTask = await this.downloadAdapter.createDownloadTask(resource, options);
-      this.downloadManager.addTask(downloadTask);
-      debugger;
-    } catch (error) {
-      console.error(error);
-      debugger;
+  async addTask(resource, options, downloadOptions = { redownload: false }) {
+    const downloadTask = await this.downloadAdapter.createDownloadTask(resource, options);
+
+    if (downloadOptions.redownload === true) {
+      this.downloadManager.deleteTask(downloadTask.id);
     }
+
+    if (downloadTask.type === 'PIXIV_UGOIRA') {
+      if (options.ugoiraConvertType.toLowerCase() === 'gif') {
+        downloadTask.generator = new GifGenerator();
+      } else {
+        throw new RuntimeError('NOT_SUPPORT_CONVERT_TYPE');
+      }
+    }
+
+    await this.downloadManager.addTask(downloadTask);
   }
 }
 
