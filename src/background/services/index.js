c3030436cccb8fcfe3bb6201935a2381ae4b323b
leoding86 <leoding86@gmail.com>
2024-08-12 19:42:25 +0800

    add download file channel service

--- src/background/services/index.js
+++ src/background/services/index.js
@@ -1,4 +1,5 @@
 import BadgeService from "./BadgeService";
+import DownloadFileService from './DownloadFileService';
 import DownloadService from "./DownloadService";
 import HistoryService from "./HistoryService";
 import LogService from "./LogService";
@@ -7,6 +8,7 @@ import TabService from "./TabService";
 
 export default {
   BadgeService,
+  DownloadFileService,
   DownloadService,
   HistoryService,
   LogService,
