a12c6a01e5c9ae5bc7a22a4b5963be0a74e1446c
leoding86 <leoding86@gmail.com>
2024-09-08 14:25:11 +0800

    update

--- src/modules/GlobalSettings.js
+++ src/modules/GlobalSettings.js
@@ -1,3 +1,12 @@
+/**
+ * @typedef GlobalSettings
+ * @property {number} downloadSaveMode
+ * @property {number} globalZipMultipleImages
+ * @property {number} downloadSaveMode
+ *
+ * @param {*} fallback
+ * @returns {GlobalSettings}
+ */
 const GlobalSettings = (fallback) => {
   if (window.$app) {
     return window.$app.settings;
