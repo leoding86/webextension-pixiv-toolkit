a6e3b938ccf2e9af1ffbfa02312bc3dc8eaafd33
leoding86 <leoding86@gmail.com>
2024-09-08 13:52:15 +0800

    update

--- src/options_page/components/NewSettings.vue
+++ src/options_page/components/NewSettings.vue
@@ -1,164 +1,45 @@
+<!--
+ * @Author: Leo Ding <leoding86@msn.com>
+ * @Date: 2024-08-11 10:45:18
+ * @LastEditors: Leo Ding <leoding86@msn.com>
+ * @LastEditTime: 2024-09-08 13:27:35
+ * @FilePath: \webextension-pixiv-toolkit\src\options_page\components\NewSettings.vue
+-->
 <template>
-    <v-card class="new-settings">
-        <v-card-text>
-            <p style="font-size:16px;font-weight: 500;">{{ tl('_new_settings') }}</p>
-            <p style="font-size:16px;font-weight: 500;">{{ tl('_download_task_settings') }}</p>
+  <v-card class="new-settings">
+    <v-card-text>
+      <p style="font-size:16px;font-weight: 500;">{{ tl('_new_settings') }}</p>
 
-            <v-card>
-                <v-card-text>
-                    <download-mode></download-mode>
-
-                    <v-list-tile>
-                        <v-list-tile-content>
-                        <v-list-tile-title>{{ tl('_dont_create_work_folder') }}</v-list-tile-title>
-                        </v-list-tile-content>
-                        <v-list-tile-action>
-                        <v-select :items="dontCreateWorkFolderOptions"
-                            v-model="dontCreateWorkFolder"
-                            style="width:175px;"
-                        ></v-select>
-                        </v-list-tile-action>
-                    </v-list-tile>
-
-                    <v-list-tile v-if="[1,2].indexOf(dontCreateWorkFolder) > -1">
-                        <v-list-tile-content>
-                        <v-list-tile-title>{{ tl('_combin_work_and_image_rename_rule_when_dont_create_work_folder') }}</v-list-tile-title>
-                        </v-list-tile-content>
-                        <v-list-tile-action>
-                        <v-select :items="zipMultipleImagesOptions"
-                            v-model="combinWRRuleAndIRRuleWhenDontCreateWorkFolder"
-                            style="width:150px;"
-                        ></v-select>
-                        </v-list-tile-action>
-                    </v-list-tile>
-                </v-card-text>
-            </v-card>
-        </v-card-text>
-    </v-card>
+      <v-list two-line>
+        <download-mode></download-mode>
+        <DownloadSaveMode />
+        <ZipDownloads />
+        <DontCreateWorkFolder />
+      </v-list>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
 import DownloadMode from './options/option-items/DownloadMode.vue';
+import DownloadSaveMode from './options/option-items/DownloadSaveMode.vue';
+import ZipDownloads from './options/option-items/ZipDownloads.vue';
+import DontCreateWorkFolder from './options/option-items/DontCreateWorkFolder.vue';
 
 export default {
   name: 'global-task-setting',
 
   components: {
-    'download-mode': DownloadMode
-  },
-
-  data() {
-    return {
-      pageNumberStartWithOne: 0,
-
-      pageNumberLength: 0,
-
-      zipMultipleImages: 1,
-
-      dontCreateWorkFolder: 0,
-
-      combinWRRuleAndIRRuleWhenDontCreateWorkFolder: 0
-    };
-  },
-
-  computed: {
-    pageNumberLengthOptions() {
-      return [{
-        text: this.tl('_disable'),
-        value: 0,
-      }, {
-        text: this.tl('_dynamic'),
-        value: -1,
-      }, {
-        text: '2',
-        value: 2
-      }, {
-        text: '3',
-        value: 3
-      }, {
-        text: '4',
-        value: 4
-      }];
-    },
-
-    pageNumberStartWithOneOptions() {
-      return [{
-        text: this.tl('_enable'),
-        value: 1,
-      }, {
-        text: this.tl('_disable'),
-        value: 0,
-      }]
-    },
-
-    zipMultipleImagesOptions() {
-      return [{
-        text: this.tl('_enable'),
-        value: 1,
-      }, {
-        text: this.tl('_disable'),
-        value: 0
-      }]
-    },
-
-    dontCreateWorkFolderOptions() {
-      return [{
-        text: this.tl('_disable'),
-        value: 0
-      }, {
-        text: this.tl('_when_there_is_only_one_image'),
-        value: 1
-      }, {
-        text: this.tl('_always'),
-        value: 2
-      }]
-    }
-  },
-
-  watch: {
-    pageNumberStartWithOne(val) {
-      browser.storage.local.set({
-        globalTaskPageNumberStartWithOne: val
-      });
-    },
-
-    pageNumberLength(val) {
-      browser.storage.local.set({
-        globalTaskPageNumberLength: val
-      });
-    },
-
-    zipMultipleImages(val) {
-      browser.storage.local.set({
-        globalZipMultipleImages: val
-      });
-    },
-
-    dontCreateWorkFolder(val) {
-      browser.storage.local.set({
-        dontCreateWorkFolder: val
-      });
-    },
-
-    combinWRRuleAndIRRuleWhenDontCreateWorkFolder(val) {
-      browser.storage.local.set({
-        combinWRRuleAndIRRuleWhenDontCreateWorkFolder: val
-      });
-    }
-  },
-
-  created() {
-    this.pageNumberStartWithOne = this.browserItems.globalTaskPageNumberStartWithOne;
-    this.pageNumberLength = this.browserItems.globalTaskPageNumberLength;
-    this.zipMultipleImages = this.browserItems.globalZipMultipleImages;
-    this.dontCreateWorkFolder = this.browserItems.dontCreateWorkFolder;
-    this.combinWRRuleAndIRRuleWhenDontCreateWorkFolder = this.browserItems.combinWRRuleAndIRRuleWhenDontCreateWorkFolder;
-  },
+    'download-mode': DownloadMode,
+    DownloadSaveMode,
+    ZipDownloads,
+    DontCreateWorkFolder
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 .new-settings {
-    margin: 10px 0;
+  margin: 10px 0;
 }
 </style>
--- src/options_page/components/UpdateNotice.vue
+++ src/options_page/components/UpdateNotice.vue
@@ -1,3 +1,10 @@
+<!--
+ * @Author: Leo Ding <leoding86@msn.com>
+ * @Date: 2024-08-11 10:45:18
+ * @LastEditors: Leo Ding <leoding86@msn.com>
+ * @LastEditTime: 2024-09-08 13:47:38
+ * @FilePath: \webextension-pixiv-toolkit\src\options_page\components\UpdateNotice.vue
+-->
 <template>
   <v-dialog
     v-model="show"
@@ -11,6 +18,16 @@
       <v-card-text>
         <div style="font-size:20px;font-weight:700;margin-bottom:10px;text-align:center;">✨{{ tl('_sponsors') }}✨</div>
         <sponsors-card></sponsors-card>
+
+        <div style="height: 10px;"></div>
+
+        <v-card>
+          <v-card-text>
+            <p style="font-size: 14px;">Some thing to say...</p>
+            <p style="font-size: 14px;margin-bottom:0;">{{ tl('_startup_message') }}</p>
+          </v-card-text>
+        </v-card>
+
         <new-settings></new-settings>
         <change-log></change-log>
       </v-card-text>
